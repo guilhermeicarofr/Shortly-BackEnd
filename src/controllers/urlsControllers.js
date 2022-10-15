5fe8acab36c21f9a95680ec33bf83ab6500b6327
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-15 03:42:17 -0300

    feat!(urls) - find one url by id feature added

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -2,15 +2,16 @@ import { nanoid } from 'nanoid';
 
 import { db } from '../databases/database.js';
 
-async function createShortUrl (req,res) {
+async function createShortUrl(req,res) {
 
     const { user } = res.locals;
-    const url = res.locals.valid;
+    const { url } = res.locals.valid;
 
     const shortUrl = nanoid();
 
     try {
-        await db.query('INSERT INTO links (user_id, full_url, short_url) VALUES ($1, $2, $3);', [ user.id, url, shortUrl ]);
+        await db.query(`INSERT INTO links (user_id, full_url, short_url)
+                        VALUES ($1, $2, $3);`, [ user.id, url, shortUrl ]);
         res.status(201).send({ shortUrl });
     } catch (error) {
         console.log(error);
@@ -18,4 +19,26 @@ async function createShortUrl (req,res) {
     }
 }
 
-export { createShortUrl };
+async function findOneUrl(req,res) {
+    
+    const id = req.params?.id;
+
+    try {
+        const link = await db.query(`SELECT links.id, links.short_url AS "shortUrl", links.full_url AS "url"
+                                    FROM links
+                                    WHERE links.id=$1
+                                    LIMIT 1;`, [ id ]);
+        
+        if(!link.rows[0]) {
+            console.log('link not found');
+            res.status(404).send('link not found');
+        }
+
+        res.status(200).send(link.rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export { createShortUrl, findOneUrl };
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import { validateAuthToken } from '../middlewares/tokenAuthenticationMiddlewares.js';
 import { validateSchema } from '../middlewares/schemaValidationMiddlewares.js';
 import { urlSchema } from '../schemas/urlsSchemas.js';
-import { createShortUrl } from '../controllers/urlsControllers.js';
+import { createShortUrl, findOneUrl } from '../controllers/urlsControllers.js';
 
 const urlsRouter = Router();
+
+
+
+urlsRouter.get('/urls/:id', findOneUrl);
 
 urlsRouter.use(validateAuthToken);
 urlsRouter.post('/urls/shorten', (req,res,next) => validateSchema(urlSchema,req,res,next), createShortUrl);
